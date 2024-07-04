39b91cd96fcc5624eb63aae893febd73c82b602f
oTerra <tdfvini@gmail.com>
2024-07-03 21:57:57 -0300

    Iniciando o uso do pydantic para criar schemas

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -3,10 +3,12 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
+from fast_zero.schemas import Message
+
 app = FastAPI()
 
 
-@app.get('/', status_code=HTTPStatus.OK)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
 
