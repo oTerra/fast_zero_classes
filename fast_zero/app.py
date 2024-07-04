995b703a124183a335bf9d4036eda325f7b95a61
oTerra <tdfvini@gmail.com>
2024-07-03 21:30:01 -0300

    Criando endpoint que retorna 'Olá mundo!' no formato de HTML

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,16 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def html():
+    return '<h1>Olá mundo!</h1>'
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,11 @@ def test_read_root_must_return_ok_and_hello_world():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
+
+
+def test_html_must_return_html_with_hello_world():
+    client = TestClient(app)
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '<h1>Olá mundo!</h1>'
